b9606c28d4670b8d7a9d1ee7b7aca85394080a11
GitGinocchio <106624847+GitGinocchio@users.noreply.github.com>
2026-06-07 20:15:01 +0200

    Update prelude.rs

--- src/prelude.rs
+++ src/prelude.rs
@@ -15,8 +15,6 @@ pub use crate::models::command::{
 pub use crate::models::components::{Component, data::ComponentData};
 pub use crate::models::interaction::Interaction;
 pub use crate::models::modal::{Modal, data::ModalData};
-
-pub use crate::error::{Error, Result};
 pub use crate::traits::resolvable::Resolvable;
 
 pub use crate::bot::Bot;
